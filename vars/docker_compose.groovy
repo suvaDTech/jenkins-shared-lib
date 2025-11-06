e67e209be6b0844608df3910d585135bbea743e2
suvaDTech <70432957+suvaDTech@users.noreply.github.com>
2025-11-06 23:37:03 +0530

    Update docker_compose.groovy

--- vars/docker_compose.groovy
+++ vars/docker_compose.groovy
@@ -3,5 +3,6 @@ def call(){
       docker ps -q --filter "publish=80" | xargs -r docker stop
       docker ps -aq --filter "publish=80" | xargs -r docker rm
      '''
-  sh "docker-compose down && docker-compose up -d"
+  sh "docker compose down || true"
+  sh "docker compose up -d"
 }
