98af34c9acd35de54b056f48259e1567c72d0cdf
suvaDTech <70432957+suvaDTech@users.noreply.github.com>
2025-11-06 23:28:29 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,5 +2,5 @@ def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'dockerhubCred', passwordVariable: 'DOCKER_USER', usernameVariable: 'DOCKER_PASS')]) {
       sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}"
   }
-  sh "docker push ${DOCKER_USER}/${Project}:${ImageTag}"
+  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
